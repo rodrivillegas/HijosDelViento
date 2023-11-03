5c9545a8d5e6a858f518db075b4f5076ca169be4
Rodri Villegas <drvillegasrodrigo@gmail.com>
2023-11-03 13:55:53 -0300

    cambios

--- js/haceTuOrdenComidaDelivery.js
+++ js/haceTuOrdenComidaDelivery.js
@@ -646,7 +646,7 @@ function cambiarBoton(
       return; // Detener la ejecución si no se selecciona una cantidad válida
     }
     const horaActual = new Date().getHours();
-    const horaInicioAviso = 23; // Hora de inicio para mostrar el aviso (23:00 horas)
+    const horaInicioAviso = 9; // Hora de inicio para mostrar el aviso (23:00 horas)
     const horaFinAviso = 6; // Hora de finalización para mostrar el aviso (06:00 horas)
     if (
       (categoria === "LOMOS" || nombre === "BAGUETTE DE ENTRAÑA") &&
